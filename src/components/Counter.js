7fdb00fe2eb2d7a8324edfa5cba0ee36ff3c8d81
Hemanshu Mahajan <hemanshumahajan@Hemanshus-MacBook-Air.local>
2021-09-22 16:34:49 +0530

    Practice Redux Toolkit

---
+++ src/components/Counter.js
@@ -0,0 +1,38 @@
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/counter-slice";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const toggle = useSelector((state) => state.counter.showCounter);
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+  const incrementByFiveHandler = () => {
+    dispatch(counterActions.incrementByFive(5));
+  };
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {toggle && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={incrementByFiveHandler}>Increase By 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
